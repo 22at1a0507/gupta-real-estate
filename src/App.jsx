d77f6b6a787d7ae0741351e8e7d9c232b134fa83
Athuluru Shreya Sree <159885409+22at1a0507@users.noreply.github.com>
2026-09-01 16:33:57 +0530

    Adjust avatar image position in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -645,7 +645,7 @@ const styles = {
     width: '100%',
     height: '100%',
     objectFit: 'cover',
-    objectPosition: '50% 65%',
+    objectPosition: '55% 60%',
   },
   agentName: {
     fontSize: '24px',
