7ab317859425a4fffc98f62b6f1b1f527dabe0f9
Athuluru Shreya Sree <159885409+22at1a0507@users.noreply.github.com>
2026-09-01 16:46:24 +0530

    Adjust objectPosition for avatarImage styling

--- src/App.jsx
+++ src/App.jsx
@@ -645,7 +645,7 @@ const styles = {
     width: '100%',
     height: '100%',
     objectFit: 'cover',
-    objectPosition: '50% 60%',
+    objectPosition: '60% 50%',
   },
   agentName: {
     fontSize: '24px',
