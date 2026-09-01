9eaff25d6dc3dadfa0ad290ba031c6952354318c
Athuluru Shreya Sree <159885409+22at1a0507@users.noreply.github.com>
2026-09-01 16:49:58 +0530

    Change avatar image object position
    Adjust avatar image position for better alignment.

--- src/App.jsx
+++ src/App.jsx
@@ -645,7 +645,7 @@ const styles = {
     width: '100%',
     height: '100%',
     objectFit: 'cover',
-    objectPosition: '60% 60%',
+    objectPosition: '50% 55%',
   },
   agentName: {
     fontSize: '24px',
