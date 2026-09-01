d2e376ad09aa37c6e150177b8fe1bb1fb042c473
Athuluru Shreya Sree <159885409+22at1a0507@users.noreply.github.com>
2026-09-01 16:37:18 +0530

    Adjust avatar image position to center

--- src/App.jsx
+++ src/App.jsx
@@ -645,7 +645,7 @@ const styles = {
     width: '100%',
     height: '100%',
     objectFit: 'cover',
-    objectPosition: '50% 55%',
+    objectPosition: '50% 50%',
   },
   agentName: {
     fontSize: '24px',
